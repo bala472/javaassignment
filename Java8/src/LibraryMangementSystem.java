755e66ed36243c78c1d296887a956131c3bf98b1
bala472 <160371380+bala472@users.noreply.github.com>
2024-03-27 19:06:58 +0530

    Add files via upload

---
+++ Java8/src/LibraryMangementSystem.java
@@ -0,0 +1,44 @@
+public class LibraryMangementSystem {
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void login(){}
+    public void register(){}
+    public void logout(){}
+}
+class User{
+    private String name;
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
+
