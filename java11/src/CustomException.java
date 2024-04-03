ea55ccaa4fbf94daff9f299f3528c390ed811e08
bala472 <160371380+bala472@users.noreply.github.com>
2024-04-03 16:16:56 +0530

    Add files via upload

--- java11/src/CustomException.java
+++ java11/src/CustomException.java
@@ -1,13 +1,20 @@
-public class CustomException {
+class  CustomException extends Exception{
+    public CustomException(String message){
+        super(message);
+    }
+}
+class UseCustomException {
     public static void main(String[] args) {
         try{
-            int a=1000;
-            if(a>100){
-                throw new Exception("Exception thrown successfully");
+            int age = -2;
+            if(age<0){
+                throw new CustomException("Custom Exception thrown successfully");
             }
-        }catch (Exception e){
+        }catch (CustomException e){
          //   e.printStackTrace();
-            System.out.println("Printing thrown message : "+e.getMessage());
+            System.out.println("Printing custom thrown exception message : "+e.getMessage());
+        }catch (Exception e){
+            System.out.println("Printing Exception");
         }
     }
 }
