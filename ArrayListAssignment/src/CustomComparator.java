9550a0bdb2b3caa068b4319504608b75ad27c000
bala472 <160371380+bala472@users.noreply.github.com>
2024-04-13 23:29:04 +0530

    Add files via upload

--- ArrayListAssignment/src/CustomComparator.java
+++ ArrayListAssignment/src/CustomComparator.java
@@ -1,24 +1,43 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
-class CustomComparator implements Comparator<CustomComparator> {
-    private String bookName;
-    private double price;
-    CustomComparator(String bookName,double price){
-        this.bookName=bookName;
-        this.price = price;
-    }
-    public int compare(CustomComparator one, CustomComparator two){
+class CustomComparator implements Comparator<Book> {
+    
+    public int compare(Book one, Book two){
         return Integer.compare((int)one.price,(int)two.price);
     }
 
-    @Override
-    public String toString() {
-        return "Book Name : "+bookName+" Price : "+price;
-    }
+}
 
+class Book {
+    String bookName;
+    double price;
+    Book(String bookName,double price){
+        this.bookName=bookName;
+        this.price = price;
+    }
+    
+    
+    
+}
+class ImplementBookComparator{
     public static void main(String[] args) {
-        CustomComparator book1 = new CustomComparator("bala",2000);
-        CustomComparator book2 = new CustomComparator("sala",100);
-        for (CustomComparator custom : )
+        ArrayList<Book> bookList = new ArrayList<>();
+        bookList.add(new Book("Ponniyin Selvan",2000));
+        bookList.add(new Book("Atomic Habits",100));
+        bookList.add(new Book("Bagavat geeta", 500));
+        bookList.add(new Book("Psychology of Money", 1850));
+        for(Book k : bookList){
+            System.out.println("BookName : "+k.bookName +" Price : "+ k.price);
+        }
+        Collections.sort(bookList, new CustomComparator());
+        System.out.println("\nAfter sorting using CustomComparator class\n");
+        for(Book k : bookList){
+            System.out.println("BookName : "+k.bookName +" Price : "+ k.price);
+        }
     }
 }
+
