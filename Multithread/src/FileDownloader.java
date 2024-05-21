80ed0d960ce04b546acacd1a3b883dd33c7bafe7
bala472 <160371380+bala472@users.noreply.github.com>
2024-05-21 12:56:26 +0530

    Add files via upload

--- Multithread/src/FileDownloader.java
+++ Multithread/src/FileDownloader.java
@@ -9,53 +9,44 @@ public class FileDownloader extends Thread {
     private String destinationPath;
     private volatile boolean stopped;
     private int progress;
-
     public FileDownloader(String fileUrl, String destinationPath) {
         this.fileUrl = fileUrl;
         this.destinationPath = destinationPath;
         this.stopped = false;
         this.progress = 0;
     }
-
     public void stopDownload() {
         this.stopped = true;
     }
-
     public int getProgress() {
         return progress;
     }
 
-    @Override
     public void run() {
         try {
             URL url = new URL(fileUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             long fileSize = connection.getContentLengthLong();
-
             try (InputStream inputStream = connection.getInputStream();
                  FileOutputStream outputStream = new FileOutputStream(destinationPath)) {
-
                 byte[] buffer = new byte[1024];
                 int bytesRead;
                 long totalBytesRead = 0;
-
                 while ((bytesRead = inputStream.read(buffer)) != -1) {
                     if (stopped) {
                         System.out.println("Download interrupted.");
                         return;
                     }
-
                     outputStream.write(buffer, 0, bytesRead);
                     totalBytesRead += bytesRead;
-
                     if (fileSize > 0) {
                         int newProgress = (int) ((totalBytesRead * 100) / fileSize);
                         if (newProgress != progress) {
                             progress = newProgress;
                             System.out.println("Downloaded " + progress + "%");
                         }
-                    }
-                }
+                     }
+                     }
 
                 System.out.println("Download completed.");
             }
@@ -67,10 +58,8 @@ public class FileDownloader extends Thread {
     public static void main(String[] args) {
         String fileUrl = "https://freetestdata.com/wp-content/uploads/2023/04/10.5-MB-scaled.bmp";
         String destinationPath = "downloaded_file.bmp";
-
         FileDownloader downloader = new FileDownloader(fileUrl, destinationPath);
         downloader.start();
-
         try {
             System.out.println("Press Enter to interrupt the download...");
             System.in.read();
